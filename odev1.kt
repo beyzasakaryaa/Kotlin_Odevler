c0e39f8213f97d87a38f944f04c29a3d966a4402
Beyza Sakarya <153944459+beyzasakaryaa@users.noreply.github.com>
2025-04-25 15:21:27 +0300

    Odev 1 Yuklendi.

--- odev1.kt
+++ odev1.kt
@@ -1,4 +1,42 @@
-package com.example.odev1
+package com.example.kotlin.odevler
 
-class odev1 {
+fun main(){
+    var sehir = "Istanbul"
+    var ulke = "Turkiye"
+    var tel = "05559865455"
+    val postaKodu = 37863
+    var mail = "beyzasakarya@gmail.com"
+    var meslek = "Muhendis"
+    var stokMiktari = 100
+    var musteriAdi = "Ayse Ozgur"
+    var bakiye = 15000
+    var dogumGunu = "17.01.2003"
+    var maas = 75000
+    var medeniDurum = "Bekar"
+    var urunYorum = "Kaliteli.Tavsiye ederim."
+    var odemeTarihi = "07.10.2025"
+    var siparisAdedi = 500
+    var arabaModeli = "Ford"
+    var kitapAdi = "Denizler Altında Yirmi Bin Fersah"
+    val odaSayisi = 5
+    val yemekFiyati = 350
+    val urunPuani = 4.4
+    var renk = "Kırmızı"
+    var renkKodu = "#23546"
+    var telModeli = "Iphone"
+    var agirlik = 125
+    var sokakAdi = "Kelebek Sokak"
+    var faturaAdresi = "Umraniye / İstanbul"
+
+    println(ulke)
+    println(renkKodu)
+    println("Ülke : $ulke")
+    println("Oda Sayısı : $odaSayisi")
+    println("Kitap Adı : $kitapAdi")
+    println("Araba Modeli : $arabaModeli")
+    println("Sipariş Adedi : $siparisAdedi")
+    println("Agirlik : $agirlik")
+    println("Musteri Adi : $musteriAdi")
+    println("Bakiye : $bakiye")
+
 }
